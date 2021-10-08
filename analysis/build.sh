51b034e588c4ac33308c510e594b46ddee8e6a17
Meinte Boersma <meinte.boersma@gmail.com>
2021-10-08 11:40:43 +0200

    put scripts in 1

--- analysis/build.sh
+++ analysis/build.sh
@@ -5,3 +5,8 @@ rm $HTML_DOWNLOAD
 
 # TODO  all-rules.json has ASCII encoding, with \u<hex> instead of UTF-8 encoded characters: convert to proper UTF8
 
+cat tmp/all-rules.json | jq 'map((.Identifier|capture("(?<t>[A-Z]+)-(?<c>[A-Z]+)-(?<n>[0-9]+)")) + .)' > tmp/all-rules-exploded-IDs.json
+rm -rf per-country/*
+node src/split-rules.js
+node src/copy-EU-rules.js
+
--- analysis/src/process-downloaded-rules.sh
+++
@@ -1,1 +0,0 @@
-cat tmp/all-rules.json | jq 'map((.Identifier|capture("(?<t>[A-Z]+)-(?<c>[A-Z]+)-(?<n>[0-9]+)")) + .)' > tmp/all-rules-exploded-IDs.json
